7f8b901113f94405880c57af01dec9645566b41d
Gusta Acosta <acostag.123456@gmail.com>
2024-10-03 20:57:32 -0300

    bbdd clever-cloud y variables de entorno

--- armasDS/src/main/java/com/armasDS/ArmasDsApplication.java
+++ armasDS/src/main/java/com/armasDS/ArmasDsApplication.java
@@ -1,5 +1,6 @@
 package com.armasDS;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ArmasDsApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL"));
+		System.setProperty("DATABASE_USERNAME", dotenv.get("DATABASE_USERNAME"));
+		System.setProperty("DATABASE_PASSWORD", dotenv.get("DATABASE_PASSWORD"));
 		SpringApplication.run(ArmasDsApplication.class, args);
 	}
 
