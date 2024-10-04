596e25e1ad2cc89af1eed09d36da9506fba664b8
Gusta Acosta <acostag.123456@gmail.com>
2024-10-03 21:29:43 -0300

    bbdd clever-cloud y variables de entorno 3

--- armasDS/src/main/java/com/armasDS/ArmasDsApplication.java
+++ armasDS/src/main/java/com/armasDS/ArmasDsApplication.java
@@ -8,10 +8,26 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ArmasDsApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-		System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL"));
-		System.setProperty("DATABASE_USERNAME", dotenv.get("DATABASE_USERNAME"));
-		System.setProperty("DATABASE_PASSWORD", dotenv.get("DATABASE_PASSWORD"));
+		String env = System.getenv("ENV");
+		if (env == null || env.equals("local")) {
+//			Dotenv dotenv = Dotenv.load();
+//			System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL"));
+//			System.setProperty("DATABASE_USERNAME", dotenv.get("DATABASE_USERNAME"));
+//			System.setProperty("DATABASE_PASSWORD", dotenv.get("DATABASE_PASSWORD"));
+			Dotenv dotenv = Dotenv.load();
+			String databaseUrl = dotenv.get("DATABASE_URL");
+			String databaseUsername = dotenv.get("DATABASE_USERNAME");
+			String databasePassword = dotenv.get("DATABASE_PASSWORD");
+
+			System.setProperty("DATABASE_URL", databaseUrl);
+			System.setProperty("DATABASE_USERNAME", databaseUsername);
+			System.setProperty("DATABASE_PASSWORD", databasePassword);
+
+			// Log para ver los valores
+			System.out.println("DATABASE_URL: " + databaseUrl);
+			System.out.println("DATABASE_USERNAME: " + databaseUsername);
+		}
+
 		SpringApplication.run(ArmasDsApplication.class, args);
 	}
 
